239360b48a853ef3efffed5648030dc3dbadf618
GitHub Action <action@github.com>
2021-09-16 13:33:09 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/demiusacademius/hibernator/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.demiusacademius.hibernator.services
+
+import com.github.demiusacademius.hibernator.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
